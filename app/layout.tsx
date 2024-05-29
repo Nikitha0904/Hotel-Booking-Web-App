04af054a76153b685c141e3ed8bc5c55857ac29b
nikitha <nikithametta0904@gmail.com>
2024-05-30 03:00:39 +0530

    hotel list added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,64 @@
+// import type { Metadata } from "next";
+// import { Inter } from "next/font/google";
+// import "./globals.css";
+// import { ClerkProvider } from "@clerk/nextjs";
+// import NavBar from "@/components/layout/NavBar";
+// import { ThemeProvider } from "@/components/theme-provider"
+// import Container from "@/components/container";
+// import { Toaster } from "@/components/ui/toaster"
+
+
+// const inter = Inter({ subsets: ["latin"] });
+
+// export const metadata: Metadata = {
+//   title: "Hotel Booking Web App",
+//   description: "Book a hotel of your choice",
+//   icons: '/logo.png' 
+// };
+
+// export default function RootLayout({
+//   children,
+// }: Readonly<{
+//   children: React.ReactNode;
+// }>) {
+//   return (
+//     <ClerkProvider>
+//       <html lang="en" suppressHydrationWarning>
+
+//         <body className={inter.className}>
+//           <ThemeProvider
+//             attribute="class"
+//             defaultTheme="system"
+//             enableSystem
+//             disableTransitionOnChange>
+//             <Toaster />
+//             <main className="flex flex-col min-h-screen bg-secondary">
+//               <NavBar />
+//               <section className="flex-grow">
+//                 <Container>
+//                   {children}
+//                 </Container>
+//               </section>
+
+//             </main>
+//           </ThemeProvider>
+//         </body>
+//       </html>
+//     </ClerkProvider>
+//   );
+// }
+
+
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { ClerkProvider } from "@clerk/nextjs";
+import {
+  ClerkProvider,
+  SignInButton,
+  SignedIn,
+  SignedOut,
+  UserButton
+} from '@clerk/nextjs'
 import NavBar from "@/components/layout/NavBar";
 import { ThemeProvider } from "@/components/theme-provider"
 import Container from "@/components/container";
@@ -13,7 +70,7 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Hotel Booking Web App",
   description: "Book a hotel of your choice",
-  icons: '/logo.png'
+  icons: '/logo.png' 
 };
 
 export default function RootLayout({
@@ -24,10 +81,8 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en" suppressHydrationWarning>
-
         <body className={inter.className}>
-          <ThemeProvider
-            attribute="class"
+          <ThemeProvider attribute="class"
             defaultTheme="system"
             enableSystem
             disableTransitionOnChange>
@@ -39,11 +94,11 @@ export default function RootLayout({
                   {children}
                 </Container>
               </section>
-
             </main>
           </ThemeProvider>
         </body>
       </html>
     </ClerkProvider>
+
   );
 }
