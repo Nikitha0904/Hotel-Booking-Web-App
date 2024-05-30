f4d02845fe1ada965926c979387a2beb54e9ed64
nikitha <nikithametta0904@gmail.com>
2024-05-31 00:16:31 +0530

    Navbar modified

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,69 +1,13 @@
-// import type { Metadata } from "next";
-// import { Inter } from "next/font/google";
-// import "./globals.css";
-// import { ClerkProvider } from "@clerk/nextjs";
-// import NavBar from "@/components/layout/NavBar";
-// import { ThemeProvider } from "@/components/theme-provider"
-// import Container from "@/components/container";
-// import { Toaster } from "@/components/ui/toaster"
-
-
-// const inter = Inter({ subsets: ["latin"] });
-
-// export const metadata: Metadata = {
-//   title: "Hotel Booking Web App",
-//   description: "Book a hotel of your choice",
-//   icons: '/logo.png' 
-// };
-
-// export default function RootLayout({
-//   children,
-// }: Readonly<{
-//   children: React.ReactNode;
-// }>) {
-//   return (
-//     <ClerkProvider>
-//       <html lang="en" suppressHydrationWarning>
-
-//         <body className={inter.className}>
-//           <ThemeProvider
-//             attribute="class"
-//             defaultTheme="system"
-//             enableSystem
-//             disableTransitionOnChange>
-//             <Toaster />
-//             <main className="flex flex-col min-h-screen bg-secondary">
-//               <NavBar />
-//               <section className="flex-grow">
-//                 <Container>
-//                   {children}
-//                 </Container>
-//               </section>
-
-//             </main>
-//           </ThemeProvider>
-//         </body>
-//       </html>
-//     </ClerkProvider>
-//   );
-// }
-
-
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import {
-  ClerkProvider,
-  SignInButton,
-  SignedIn,
-  SignedOut,
-  UserButton
-} from '@clerk/nextjs'
+  ClerkProvider
+} from '@clerk/nextjs';
 import NavBar from "@/components/layout/NavBar";
-import { ThemeProvider } from "@/components/theme-provider"
+import { ThemeProvider } from "@/components/theme-provider";
 import Container from "@/components/container";
-import { Toaster } from "@/components/ui/toaster"
-
+import { Toaster } from "@/components/ui/toaster";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -82,10 +26,12 @@ export default function RootLayout({
     <ClerkProvider>
       <html lang="en" suppressHydrationWarning>
         <body className={inter.className}>
-          <ThemeProvider attribute="class"
+          <ThemeProvider
+            attribute="class"
             defaultTheme="system"
             enableSystem
-            disableTransitionOnChange>
+            disableTransitionOnChange
+          >
             <Toaster />
             <main className="flex flex-col min-h-screen bg-secondary">
               <NavBar />
@@ -99,6 +45,5 @@ export default function RootLayout({
         </body>
       </html>
     </ClerkProvider>
-
   );
 }
--- components/layout/NavBar.tsx
+++ components/layout/NavBar.tsx
@@ -10,36 +10,35 @@ import { NavMenu } from './NavMenu'
 
 const NavBar = () => {
     const router = useRouter()
-    const {userId} = useAuth()
+    const { userId } = useAuth()
+
     return (
-        <div className='sticky top-0 border border-b-primary/10 bg-secondary'>
-            
-
-            
+        <div className='sticky top-0 z-50 border-b border-b-primary/10 bg-secondary h-16'>
             <Container>
-            <div className='flex justify-between items-center'>
-                <div className='flex items-center gap-1 cursor-pointer' onClick={() => router.push('/')}>
-                    <Image src='/logo.png' alt ="logo" width={30} height={30}/>
-                    <div className='font-bold text-xl'>
-                        StayFinder
-                    </div>
-                    
-                </div>
-                <SearchInput/>
-                <div className='flex gap-3 items-center '>
-                        <div>
-                            <ModeToggle/>
-                            <NavMenu/>
+                <div className='flex justify-between items-center h-full'>
+                    <div className='flex items-center gap-1 cursor-pointer' onClick={() => router.push('/')}>
+                        <Image src='/logo.png' alt="logo" width={30} height={30} />
+                        <div className='font-bold text-xl'>
+                            StayFinder
                         </div>
+                    </div>
+                    <SearchInput />
+                    <div className='flex gap-3 items-center'>
+                        <ModeToggle />
+                        <NavMenu />
                         <UserButton afterSignOutUrl='/' />
-                        {!userId && <>
-                        
-                        <Button variant='outline' size='sm' onClick={() => router.push('/sign-in')}>Sign in</Button>
-                        <Button size={'sm'} onClick={() => router.push('/sign-up')}>Sign up</Button>
-
-                        </>}
+                        {!userId && (
+                            <>
+                                <Button variant='outline' size='sm' onClick={() => router.push('/sign-in')}>
+                                    Sign in
+                                </Button>
+                                <Button size='sm' onClick={() => router.push('/sign-up')}>
+                                    Sign up
+                                </Button>
+                            </>
+                        )}
                     </div>
-                    </div>
+                </div>
             </Container>
         </div>
     )
