aa72c5a81cc31b4aefc1f1cdc7c3488d9279a8b8
nikitha <nikithametta0904@gmail.com>
2024-05-31 16:16:08 +0530

    search added

--- app/my-bookings/page.tsx
+++ app/my-bookings/page.tsx
@@ -1,6 +1,6 @@
 const MyBookings = () => {
     return ( <div className="flex flex-col gap-10">
-        <h2 className="text-xl md:text-2xl font-semibold mb-6 mt-2">Here are the bookings you have made</h2>
+        <h2 className="text-xl md:text-2xl font-semibold mb-6 mt-2">Your Bookings</h2>
     </div> );
 }
  
